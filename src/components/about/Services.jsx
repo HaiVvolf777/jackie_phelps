68623edac57c0ffe404eaf06ea096e17373247f1
haider <haider.galixo.ai>
2024-12-19 18:02:25 +0500

    lastest

--- src/components/about/Services.jsx
+++ src/components/about/Services.jsx
@@ -1,87 +1,91 @@
-
-
-
 const Services = () => {
+  const Service = [
+    {
+      img: "/icons/Life Cycle.svg",
+      heading: "Federal Criminal Appeals:",
+      paragraph:
+        "Fighting for second chances and freedom for those unjustly sentenced to life in federal prison.",
+    },
 
+    {
+      img: "/icons/Non Profit Organisation.svg",
+      heading: "Non-Violent Drug Offense Defense:",
+      paragraph:
+        "Challenging sentencing disparities to ensure fair treatment under the law.",
+    },
 
-    const Service = [
-        {
-            img: '/icons/Life Cycle.svg',
-            heading: 'Federal Criminal Appeals:',
-            paragraph: 'Fighting for second chances and freedom for those unjustly sentenced to life in federal prison.',
-        },
+    {
+      img: "/icons/Mind Map.svg",
+      heading: "Sentencing Disparities Representation:",
+      paragraph: "Addressing systemic inequities in criminal sentencing.",
+    },
 
-        {
+    {
+      img: "/icons/Colorado State Capitol.svg",
+      heading: "Federal & State Clemency Applications:",
+      paragraph: "Advocating for mercy and justice through clemency petitions.",
+    },
 
-            img: '/icons/Non Profit Organisation.svg',
-            heading: 'Non-Violent Drug Offense Defense:',
-            paragraph: 'Challenging sentencing disparities to ensure fair treatment under the law.',
-        },
+    {
+      img: "/icons/Upgrade.svg",
+      heading: "Compassionate Release Motions:",
+      paragraph:
+        "Securing early release for individuals with compelling circumstances.",
+    },
 
-        {
-            img: '/icons/Mind Map.svg',
-            heading: 'Sentencing Disparities Representation:',
-            paragraph: 'Addressing systemic inequities in criminal sentencing.',
-        },
+    {
+      img: "/icons/Post Stamp.svg",
+      heading: "Post-Conviction Litigation:",
+      paragraph:
+        "  Strategically navigating complex cases to achieve favorable outcomes. ",
+    },
+  ];
 
-        {
-            img: '/icons/Colorado State Capitol.svg',
-            heading: 'Federal & State Clemency Applications:',
-            paragraph: 'Advocating for mercy and justice through clemency petitions.',
-        },
-
-        {
-            img: '/icons/Upgrade.svg',
-            heading: 'Compassionate Release Motions:',
-            paragraph: 'Securing early release for individuals with compelling circumstances.',
-        },
-
-        {
-            img: '/icons/Post Stamp.svg',
-            heading: 'Post-Conviction Litigation:',
-            paragraph: '  Strategically navigating complex cases to achieve favorable outcomes. ',
-        }
-    ]
-
-
-    return (
-        <div className="pb-8 px-4">
-            <div className="container mx-auto text-white relative">
-                <div className="px-4 ">
-                    <h2 className="font-medium text-3xl lg:text-4xl pt-24  pb-4 text-[#39648C] uppercase text-center tracking-[5px]">
-                        Core Services Include
-                    </h2>{" "}
-                </div>
-
-                <div
-                    className="grid grid-cols-1 lg:grid-cols-3 relative gap-x-10 gap-y-24 px-4 pt-8 sm:pt-20 text-black">
-
-
-                    {
-                        Service.map((service, index) => {
-                            return (
-                                <div key={index}
-                                    className="border border-[#39648C]  shadow-2xl  transform transition duration-500 hover:scale-110">
-                                    <div className="px-3 lg:px-8 py-8  ">
-                                        <div
-                                            className="w-[106px] h-[106px] bg-[#8CA6BD]  rounded-full mt-[-5rem]  flex items-center mx-auto p-4">
-                                            <img src={service.img} alt=""
-                                                className="w-[60px] h-[60px] object-contain  mx-auto" />
-                                        </div>
-                                        <h3 className="font-bold text-[18px] px-12 text-[#39648C] text-center my-6">
-                                            {service.heading}
-                                        </h3>
-                                        <p className="text-[#444444] text-[14px] px-5 text-center font-neue">
-                                            {service.paragraph}
-                                        </p>
-                                    </div>
-                                </div>
-                            )
-                        })}
-                </div>
-            </div>
+  return (
+    <div className="pb-8 px-4">
+      <div className="container mx-auto text-white relative">
+        <div className="px-4 ">
+          <h2 className="font-medium text-3xl lg:text-4xl pt-24  pb-4 text-[#39648C] uppercase text-center tracking-[5px]">
+            Core Services Include
+          </h2>{" "}
         </div>
-    );
+
+        <div className="grid grid-cols-1 lg:grid-cols-3 relative gap-x-10 gap-y-24 px-4 pt-8 sm:pt-20 text-black">
+          {Service.map((service, index) => {
+            return (
+              <div
+                key={index}
+                className="border border-[#39648C]  shadow-2xl  transform transition duration-500 hover:scale-110"
+              >
+                <div className="px-3 lg:px-8 py-8  ">
+                  <div className="w-[106px] h-[106px] bg-[#8CA6BD]  rounded-full mt-[-5rem]  flex items-center mx-auto p-4">
+                    <img
+                      src={service.img}
+                      alt=""
+                      className="w-[60px] h-[60px] object-contain  mx-auto"
+                    />
+                  </div>
+                  <h3
+                    className={`flex flex-col items-center font-bold text-[18px] text-[#39648C] text-center my-6
+    ${index === 1 || index === 4 ? "px-10" : ""}
+    ${index === 0 || index === 5 ? "px-14" : ""}
+  `}
+                  >
+                    {service.heading}
+                    <div className="w-[70px] mt-[5px] h-[1.2px] bg-[#39648C]"></div>
+                  </h3>
+
+                  <p className="text-[#444444] text-[14px] px-5 text-center font-neue">
+                    {service.paragraph}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
--- src/components/home/Approach.jsx
+++ src/components/home/Approach.jsx
@@ -1,58 +1,76 @@
 function Approach() {
-    const updates = [
-        {
-            description: "Empathy and Understanding:",
-            detail:
-                "We see our clients as individuals, not case numbers. By understanding their unique stories, we provide personalized and meaningful advocacy.",
-            image: "/Trust.svg",
-        },
-        {
-            description: "Expert Legal Research & Strategy:",
-            detail:
-                "With a proven track record in case law research and appellate litigation, we craft compelling arguments that drive results.",
-            image: "/Popular Man.svg",
-        },
-        {
-            description: "Collaboration and Transparency:",
-            detail:
-                "We partner closely with our clients, keeping them informed and engaged every step of the way.",
-            image: "/Collaboration.svg",
-        },
+  const updates = [
+    {
+      description: "Empathy and Understanding:",
+      detail:
+        "We see our clients as individuals, not case numbers. By understanding their unique stories, we provide personalized and meaningful advocacy.",
+      image: "/Trust.svg",
+    },
+    {
+      description: "Expert Legal Research & Strategy:",
+      detail:
+        "With a proven track record in case law research and appellate litigation, we craft compelling arguments that drive results.",
+      image: "/Popular Man.svg",
+    },
+    {
+      description: "Collaboration and Transparency:",
+      detail:
+        "We partner closely with our clients, keeping them informed and engaged every step of the way.",
+      image: "/Collaboration.svg",
+    },
 
+    {
+      description: "Advocacy with Power:",
+      detail:
+        "We don’t back down from systemic challenges. Our mission is to create pathways to justice that lead tolasting impact.",
+      image: "/Handle With Care.svg",
+    },
+  ];
+  return (
+    <div className="container mx-auto py-[100px] px-4">
+      <div className="px-4 mb-16">
+        <h1 className="font-medium text-3xl lg:text-5xl text-[#39648C] tracking-[5px]">
+          OUR APPROACH
+        </h1>
+        <p className="text-xs font-neue lg:text-base text-[#39648C] font-normal w-full lg:w-[45%] mt-[40px]">
+          At Jacki Phelps Law, we believe that justice isn’t just about winning
+          cases; it’s about changing lives. Our approach is rooted in:
+        </p>
+      </div>
 
-        {
-            description: "Advocacy with Power:",
-            detail:
-                "We don’t back down from systemic challenges. Our mission is to create pathways to justice that lead tolasting impact.",
-            image: "/Handle With Care.svg",
-        },
-    ];
-    return (
-        <div className="container mx-auto py-[100px] px-4">
-            <div className="px-4 mb-16">
+      <div className="grid grid-cols-1 lg:grid-cols-4 gap-x-4 gap-y-8 px-4">
+        {updates.map((feature, index) => (
+          <div key={index}>
+            <div className="h-full flex flex-col items-start gap-y-6 gap-x-4 border border-[#39648C] py-[30px] px-[30px]">
+              {/* Image */}
+              <div className="mb-2">
+                <img
+                  src={feature.image}
+                  alt="Feature Icon"
+                  className="bg-[#8CA6BD1A] p-2"
+                />
+              </div>
 
-                <h1 className="font-medium text-3xl lg:text-5xl text-[#39648C] tracking-[5px]">OUR APPROACH</h1>
-                <p className="text-xs font-neue lg:text-base text-[#39648C] font-normal w-full lg:w-[45%] mt-[40px]">
-                    At Jacki Phelps Law, we believe that justice isn’t just about winning cases; it’s about changing lives. Our approach is rooted in:
-                </p>
+              {/* Feature Description */}
+              <p className="font-semibold text-[#39648C] text-base text-start pr-[68px]">
+                {feature.description}
+              <div className="w-[70px] mt-[5px] h-[1.2px] bg-[#39648C]"> </div>
+              </p>
+
+              {/* Feature Detail */}
+              <p
+                className={`text-[#444444] text-xs font-neue ${
+                  index === 2 ? "pr-4" : "pr-0"
+                }`}
+              >
+                {feature.detail}
+              </p>
             </div>
-
-            <div className="grid grid-cols-1 lg:grid-cols-4 gap-x-4 gap-y-8 px-4">
-                {updates.map((feature, index) => (
-                    <div key={index}>
-                        <div className="h-[100%] flex flex-col items-start gap-y-6 gap-x-4  border border-[#39648C] py-[16px] px-[16px]">
-                            <div className="mb-2"><img src={feature.image} alt="" className="bg-[#8CA6BD1A] p-4" /></div>
-                            <p className="font-semibold text-[#39648C] text-lg text-start ">
-                                {feature.description}
-
-                            </p>
-                            <p className=" text-[#444444] text-sm font-neue">{feature.detail}</p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Approach;
